18c4f4e2362d5291b2186fb4e6e736691196141e
Ralf Globisch <rglobisch@csir.co.za>
2013-02-07 10:30:59 +0100

    Added calculation of std dev

--- include/cpputil/RunningAverageQueue.h
+++ include/cpputil/RunningAverageQueue.h
@@ -20,10 +20,13 @@ public:
     if (m_max_size == 0) throw std::invalid_argument("Bad size");
   }
 
+  void clear() { m_queue.clear(); }
   bool empty() const { return m_queue.empty(); }
   std::size_t size() const { return m_queue.size(); }
 
   R getAverage() const { return m_average; }
+
+  R getStandardDeviation() const { return m_standardDeviation; }
 
   void insert(const T t)
   {
@@ -31,7 +34,17 @@ public:
     if (m_queue.size() > m_max_size)
       m_queue.pop_front();
 
+    // calc mean
     m_average = std::accumulate(m_queue.begin(), m_queue.end(), 0)/static_cast<R>(m_queue.size());
+
+    // calc std dev
+    // calc difference from mean
+    std::vector<R> diff(m_queue.size());
+    std::transform(m_queue.begin(), m_queue.end(), diff.begin(),
+                     std::bind2nd(std::minus<R>(), m_average));
+
+    R sq_sum = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+    m_standardDeviation = std::sqrt(sq_sum / m_queue.size());
   }
 
 private:
@@ -39,4 +52,5 @@ private:
   std::size_t m_max_size;
   std::deque<T> m_queue;
   R m_average;
+  R m_standardDeviation;
 };
